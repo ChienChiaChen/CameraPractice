e9ba5f493ffd9422c2ce20d0b5d8a0594740f50b
ChiaChen <s124202468@gmail.com>
2017-04-01 00:25:01 +0800

    init htc can

---
+++ app/src/main/java/com/example/chiachen/camerapractice/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.chiachen.camerapractice;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import static android.hardware.Camera.getCameraInfo;
+import static android.hardware.Camera.getNumberOfCameras;
+
+public class MainActivity extends Activity {
+	private Camera mCamera = null;
+	private CameraView mCameraView = null;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		try {
+			mCamera = Camera.open();
+		} catch (Exception e) {
+			Log.e("camera", "getMessage  : " + e.getMessage());
+		}
+
+		if (null == mCamera) {
+			Log.e("camera", "mCamera is null");
+			return;
+		}
+
+		// mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
+		// FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
+		// camera_view.addView(mCameraView);//add the SurfaceView to the layout
+
+		mCameraView = new CameraView(this, mCamera);
+		FrameLayout camera_view = (FrameLayout) findViewById(R.id.camera_view);
+		camera_view.addView(mCameraView);
+
+		findViewById(R.id.exit).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				System.exit(0);
+			}
+		});
+
+
+	}
+
+	public static Camera open() {
+		int numberOfCameras = getNumberOfCameras();
+		Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+		for (int i = 0; i < numberOfCameras; i++) {
+			getCameraInfo(i, cameraInfo);
+			if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+
+				Log.e("camera","i: "+i);
+			}
+		}
+		return null;
+	}
+}
